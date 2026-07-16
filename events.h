f74801ee5d79f098ae6baddda832f1a253cf34aa
Monish H. Giani <256585194+thelazybastard@users.noreply.github.com>
2026-07-16 18:05:34 +0700

    created events files

---
+++ events.h
@@ -0,0 +1,17 @@
+#ifndef EVENTS_H
+#define EVENTS_H
+
+namespace Events {
+    void firstRiddle();
+    void secondRiddle();
+    void thirdRiddle();
+    void fourthRiddle();
+    void fifthRiddle();
+    void sixthRiddle();
+    void seventhRiddle();
+    void eightRiddle();
+    void ninthRiddle();
+    void tenthRiddle();
+} // namespace Events
+
+#endif
